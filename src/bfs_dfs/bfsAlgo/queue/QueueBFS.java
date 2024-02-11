9f95086c090e9f72dc10a7083d620fdbb31a23e0
JeongHwan Kim <rocky990903@naver.com>
2024-02-11 21:55:21 +0900

    fix bfs 로직 수정
    수정

--- src/bfs_dfs/bfsAlgo/queue/QueueBFS.java
+++ src/bfs_dfs/bfsAlgo/queue/QueueBFS.java
@@ -18,7 +18,7 @@ public class QueueBFS {
 
     static StringBuilder sb = new StringBuilder();
 
-    static boolean []visited=new boolean[graph.length];
+    static boolean []visited=new boolean[graph.length+1];
     static Queue<Integer>queue=new LinkedList<>();
     public static void main(String[] args) {
 
@@ -28,14 +28,15 @@ public class QueueBFS {
     }
     public static void bfs(int node) {
         queue.add(node);
-
+        visited[node]=true;
         while(!queue.isEmpty()){ // 큐가 비어있을때 까지 순회
             int pop = queue.poll(); // 맨 왼쪽노드 빼기
             sb.append(pop);
-            visited[pop-1]=true;
+            //visited[pop-1]=true;
             for(int nextNode:graph[pop-1]){ // 큐에서 뺀 노드가 가지고 있는 노드들 순회
-                if(!visited[nextNode-1]&& !queue.contains(nextNode)) { // 방문한적 없으면서 큐 안에 겹치는 노드가 없는 경우
+                if(!visited[nextNode]) { // 방문한적 없는 경우
                     queue.add(nextNode);
+                    visited[nextNode] = true;
                 }
             }
         }
